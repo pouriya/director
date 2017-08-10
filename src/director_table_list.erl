846f3e46b201f0257c5acf98ab664cc705d1ee87
pouriya <pouriya.jahanbakhsh@gmail.com>
2017-08-10 19:15:38 +0430

    add module for saving children in ets

---
+++ src/director_table_list.erl
@@ -0,0 +1,218 @@
+%%% --------------------------------------------------------------------
+%%% BSD 3-Clause License
+%%%
+%%% Copyright (c) 2017-2018, Pouriya Jahanbakhsh
+%%% (pouriya.jahanbakhsh@gmail.com)
+%%% All rights reserved.
+%%%
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions
+%%% are met:
+%%%
+%%% 1. Redistributions of source code must retain the above copyright
+%%% notice, this list of conditions and the following disclaimer.
+%%%
+%%% 2. Redistributions in binary form must reproduce the above copyright
+%%% notice, this list of conditions and the following disclaimer in the
+%%% documentation and/or other materials provided with the distribution.
+%%%
+%%% 3. Neither the name of the copyright holder nor the names of its
+%%% contributors may be used to endorse or promote products derived from
+%%% this software without specific prior written permission.
+%%%
+%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+%%% FOR A  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+%%% COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+%%% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+%%% BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+%%% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+%%% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+%%% LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+%%% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%%% POSSIBILITY OF SUCH DAMAGE.
+%%% --------------------------------------------------------------------
+%% @author   Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
+%% @version  17.7.10
+%% @doc
+%%           API functions for keeping, updating and fetching
+%%           childspecs data.
+%% @end
+%% @hidden
+%% ---------------------------------------------------------------------
+
+
+-module(director_table_list).
+-author("pouriya.jahanbakhsh@gmail.com").
+
+
+%% ---------------------------------------------------------------------
+%% Exports:
+
+
+
+
+
+%% API:
+-export([create/0
+        ,insert/2
+        ,delete/2
+        ,lookup/2
+        ,lookup_by_pid/2
+        ,lookup_appended/1
+        ,combine_children/2
+        ,separate_children/2
+        ,count/1
+        ,delete_table/1
+        ,tab2list/1]).
+
+
+
+
+
+%% ---------------------------------------------------------------------
+%% Records & Macros & Includes:
+
+
+
+
+
+%% Dependencies:
+%%  #?CHILD{}
+-include("internal/director_child.hrl").
+
+
+
+
+
+%% ---------------------------------------------------------------------
+%% API functions:
+
+
+
+
+
+create() ->
+    [].
+
+
+
+
+
+
+
+delete_table(_Tab) ->
+    ok.
+
+
+
+
+
+
+
+lookup(Tab, Id) ->
+    case lists:keyfind(Id, 2, Tab) of
+        false ->
+            not_found;
+        Child ->
+            Child
+    end.
+
+
+
+
+
+
+
+count(Tab) ->
+    erlang:length(Tab).
+
+
+
+
+
+
+lookup_by_pid(Tab, Pid) ->
+    case lists:keyfind(Pid, 3, Tab) of
+        false ->
+            not_found;
+        Child ->
+            Child
+    end.
+
+
+
+
+
+
+
+lookup_appended(Tab) ->
+    [Child || #?CHILD{append = Append}=Child <- Tab, Append == true].
+
+
+
+
+
+
+
+insert(Tab, #?CHILD{id = Id}=Child) ->
+    case lists:keyfind(Id, 2, Tab) of
+        false ->
+            [Child|Tab];
+        _ ->
+            {value, _, Tab2} = lists:keytake(Id, 2, Tab),
+            [Child|Tab2]
+    end.
+
+
+
+
+
+
+
+
+delete(Tab, Id) ->
+    case lists:keyfind(Id, 2, Tab) of
+        false ->
+            Tab;
+        _ ->
+            lists:keydelete(Id, 2, Tab)
+    end.
+
+
+
+
+
+
+
+tab2list(Tab) ->
+    Tab.
+
+
+
+
+
+
+
+combine_children(DefChildSpec, Table) ->
+    AppendedChildren =
+        [director_wrapper:combine_child(director_wrapper:c2cs(Child)
+                                       ,DefChildSpec)
+            || Child <- lookup_appended(Table)],
+    [insert(Table, director_wrapper:cs2c(AppendedChild)) || AppendedChild <- AppendedChildren],
+    ok.
+
+
+
+
+
+
+
+separate_children(DefChildSpec, Table) ->
+    AppendedChildren =
+        [director_wrapper:separate_child(director_wrapper:c2cs(Child)
+                                        ,DefChildSpec)
+            || Child <- lookup_appended(Table)],
+    [insert(Table, director_wrapper:cs2c(AppendedChild)) || AppendedChild <- AppendedChildren],
+    ok.
