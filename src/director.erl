1fd43242e99c4928b83e12fd500eed7f7304f345
pouriya <pouriya.jahanbakhsh@gmail.com>
2017-09-16 11:47:03 +0430

    Version 17.9.16

--- src/director.erl
+++ src/director.erl
@@ -31,7 +31,7 @@
 %%% POSSIBILITY OF SUCH DAMAGE.
 %%% ------------------------------------------------------------------------------------------------
 %% @author   Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
-%% @version  17.9.13
+%% @version  17.9.16
 %% @doc
 %%           Director is a production-ready supervisor and manager for Erlang/Elixir processes with
 %%           focus on speed and flexibility.
